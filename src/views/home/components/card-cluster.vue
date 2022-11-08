a7c92603bf95c53749104142c407158fa00581a0
Arthur Oliveira <arthurolvmorais@gmail.com>
2022-11-07 22:40:28 -0300

    feat(clusters): list clusters from api

--- src/views/home/components/card-cluster.vue
+++ src/views/home/components/card-cluster.vue
@@ -1,13 +1,25 @@
 <template>
   <a href="#" class="border bg-blue-50 rounded-2xl px-6 py-4 hover:bg-blue-100">
-    <strong class="font-bold block mb-4 border-b border-b-blue-200">
-      Cluster Name
+    <strong class="font-bold block text-center">
+      {{ props.cluster.name }}
     </strong>
 
-    <div class="flex gap-4">
+    <!-- TODO: add some metadata -->
+    <!-- <div class="flex gap-4">
       <small>CPU: 0.1/4 cores</small>
       <small>Mem: 70 MiB/7.79 GiB</small>
       <small>Pods: 10/1231</small>
-    </div>
+    </div> -->
   </a>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  cluster: {
+    id: string;
+    name: string;
+    limits: Record<string, string>;
+    requested: Record<string, string>;
+  };
+}>();
+</script>
--- src/views/home/components/grid-clusters.vue
+++ src/views/home/components/grid-clusters.vue
@@ -1,12 +1,35 @@
 <template>
   <article class="grid grid-cols-4 gap-10 w-full">
-    <card-cluster key="1" class="group" />
-    <card-cluster key="2" class="group" />
-    <card-cluster key="3" class="group" />
-    <card-cluster key="4" class="group" />
+    <card-cluster
+      v-for="cluster in clusters"
+      :key="cluster.id"
+      :cluster="cluster"
+      class="group"
+    />
   </article>
 </template>
 
 <script setup lang="ts">
+import { useHttp } from '@/composables';
+import { computed, onMounted } from 'vue';
 import CardCluster from './card-cluster.vue';
+
+const { execute, response } = useHttp();
+
+onMounted(() => execute({ url: '/v3/clusters' }));
+
+const clusters = computed(() => {
+  const list = response.value?.data?.data;
+
+  if (!list?.length) {
+    return [];
+  }
+
+  return list.map((cluster: any) => ({
+    id: cluster.id,
+    name: cluster.name,
+    limits: cluster.limits,
+    requested: cluster.requested,
+  }));
+});
 </script>
