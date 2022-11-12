6e3cde638c69fc151085f78f0ae9f02d30b931f3
Arthur Oliveira <arthurolvmorais@gmail.com>
2022-11-12 00:40:08 -0300

    feat(deployments): create row component to show data in table row details

--- src/views/deployment/components/card-namespace.vue
+++ src/views/deployment/components/card-namespace.vue
@@ -17,59 +17,18 @@
       </thead>
 
       <tbody>
-        <tr
+        <row-deployment
           v-for="deployment in deployments"
           :key="deployment.metadata.uid"
-          class="border border-gray-200 last:border-0"
-        >
-          <td class="pl-4 py-4 w-1/4 text-left">
-            {{ deployment.metadata.name }}
-          </td>
-
-          <td class="py-4 w-2/4 text-left">
-            <span class="inline-block break-all">
-              {{ deployment.spec.template.spec.containers[0].image }}
-            </span>
-
-            <button
-              class="ml-1 inline-block"
-              title="Copy image name"
-              @click="
-                copyToClipboard(
-                  deployment.spec.template.spec.containers[0].image,
-                )
-              "
-            >
-              <x-icon
-                name="clipboard-document-list"
-                class="h-4 w-4 text-blue-500"
-              />
-            </button>
-          </td>
-
-          <td class="pr-4 py-4 w-1/4 text-right">
-            <span class="inline-block">
-              <span>{{ deployment.status.readyReplicas || 0 }}</span>
-              <span>/</span>
-              <span>{{ deployment.status.replicas || 0 }}</span>
-            </span>
-
-            <button
-              class="inline-block ml-2 bg-blue-200 px-1 py-0.5 rounded text-sm hover:bg-blue-300"
-            >
-              <span>details</span>
-              <x-icon name="chevron-double-down" class="h-4 w-4" />
-            </button>
-          </td>
-        </tr>
+          :deployment="deployment"
+        />
       </tbody>
     </table>
   </div>
 </template>
 
 <script setup lang="ts">
-// import ListDeployment from './list-deployment.vue';
-import { copyToClipboard } from '@/common/func';
+import RowDeployment from './row-deployment.vue';
 import { IDeploymentItem } from '@/types/deployment';
 
 defineProps<{
