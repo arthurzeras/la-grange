c858cc0137b0239a19f977ef2dfb36831a26a06d
Arthur Oliveira <arthurolvmorais@gmail.com>
2022-11-12 01:25:11 -0300

    feat(k8s-client): add core api in k8s client object

--- server/app/providers.mjs
+++ server/app/providers.mjs
@@ -8,7 +8,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 export const kubeconfigDir = resolve(__dirname, '..', 'kube');
 
 /**
- * @returns {Record<string, {file: string, name: string, api: k8s.AppsV1Api, kube: k8s.KubeConfig>}
+ * @returns {Record<string, {file: string, name: string, api: k8s.AppsV1Api, core: k8s.CoreV1Api, kube: k8s.KubeConfig>}
  */
 export function getClients() {
   const files = readdirSync(kubeconfigDir);
@@ -18,13 +18,14 @@ export function getClients() {
       const filepath = `${kubeconfigDir}/${file}`;
 
       const api = getApiClient(filepath);
+      const core = getCoreApiClient(filepath);
       const kube = getKubeConfigClient(filepath);
 
       const yml = readFileSync(filepath, 'utf-8');
       const kubeconfig = yaml.load(yml);
       const name = kubeconfig.clusters[0].name;
 
-      return { file, name, api, kube };
+      return { file, name, api, core, kube };
     })
     .reduce((clients, client) => ({ ...clients, [client.name]: client }), {});
 }
@@ -37,6 +38,16 @@ function getApiClient(filepath = '') {
   const client = new k8s.KubeConfig();
   client.loadFromFile(filepath);
   return client.makeApiClient(k8s.AppsV1Api);
+}
+
+/**
+ * @param {string} filepath
+ * @returns {k8s.AppsV1Api}
+ */
+function getCoreApiClient(filepath = '') {
+  const client = new k8s.KubeConfig();
+  client.loadFromFile(filepath);
+  return client.makeApiClient(k8s.CoreV1Api);
 }
 
 /**
