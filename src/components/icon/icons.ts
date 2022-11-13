ac2dd114f04db6b20070c9167b6966fdc1c04364
Arthur Oliveira <arthurolvmorais@gmail.com>
2022-11-13 13:51:59 -0300

    feat(icon): add 'information-circle' icon

--- src/components/icon/icons.ts
+++ src/components/icon/icons.ts
@@ -6,7 +6,8 @@ export type IconType = {
 export type IconNames =
   | 'chevron-double-down'
   | 'chevron-double-up'
-  | 'clipboard-document-list';
+  | 'clipboard-document-list'
+  | 'information-circle';
 
 const icons: Record<IconNames, IconType> = {
   'chevron-double-down': {
@@ -23,6 +24,13 @@ const icons: Record<IconNames, IconType> = {
     size: '24',
     paths: [
       `M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z`,
+    ],
+  },
+
+  'information-circle': {
+    size: '24',
+    paths: [
+      `M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z`,
     ],
   },
 };
